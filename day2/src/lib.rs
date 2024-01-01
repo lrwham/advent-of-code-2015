20a83ee97ff3b1c6f81b02f7334080022e2d58b7
lawtonwillingham <lwillingh@gmail.com>
2024-01-01 16:03:39 -0500

    Day 2 part 1

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,8 +1,23 @@
 pub fn part_one() -> i32 {
-    let mut sum = 0;
-
+    let input = include_str!("../input.txt");
+    let mut total = 0;
     
-    sum
+    for line in input.lines() {
+        // Copilot did most of this...
+        // I do appreciate that it's idiomatic Rust.
+        // I would not have considered the fold() method.
+        let x = line.split("x").collect::<Vec<&str>>();
+        let l = x[0].parse::<i32>().unwrap();
+        let w = x[1].parse::<i32>().unwrap();
+        let h = x[2].parse::<i32>().unwrap();
+
+        let sides = vec![l*w, w*h, h*l];
+
+        let paper_needed: i32 = sides.iter().fold(0, |acc, x| acc + 2*x) + sides.iter().min().unwrap();
+        total += paper_needed;
+    }
+
+    total
 }
 
 pub fn part_two() -> i32 {
--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,5 @@ fn main() {
     println!("Hello, world!");
     println!("Day 1: Part 1: {}",day1::part_one());
     println!("Day 1: Part 2: {}",day1::part_two());
+    println!("Day 2: Part 1: {}",day2::part_one());
 }
