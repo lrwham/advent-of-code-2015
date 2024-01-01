c37ef0f223e68fdfde470121fd4772debc5b8d7d
lawtonwillingham <lwillingh@gmail.com>
2023-12-31 23:26:59 -0500

    day1 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use day1; // Import the day1 module
+
 fn main() {
     println!("Hello, world!");
+    println!("Day 1: Part 1: {}",day1::part_one());
+    println!("Day 1: Part 2: {}",day1::part_two());
 }
